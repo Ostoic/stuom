0cf589104d58db2d6f0e77cdb7601f74dd06e446
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2024-07-04 14:29:49 -0400

    test(parse): Apply ruff linter rule E721

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -14,9 +14,9 @@ from stuom.parse import find_minimal_si, parse_duration, parse_minimal_si
 
 
 def test_parse_minimal_si():
-    assert type(find_minimal_si(Milliseconds(0.05))) == Microseconds
-    assert type(find_minimal_si(Milliseconds(5000))) == Seconds
-    assert type(find_minimal_si(Milliseconds(5))) == Milliseconds
+    assert type(find_minimal_si(Milliseconds(0.05))) is Microseconds
+    assert type(find_minimal_si(Milliseconds(5000))) is Seconds
+    assert type(find_minimal_si(Milliseconds(5))) is Milliseconds
     assert parse_minimal_si(Milliseconds(0.05)) == "50.0us"
     assert parse_minimal_si(Milliseconds(5000)) == "5.0s"
     assert parse_minimal_si(Seconds(0.01)) == "10.0ms"

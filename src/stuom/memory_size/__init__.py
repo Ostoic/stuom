6710ba0bca1ae48dab86d731e4af382d8fecbcac
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2024-07-04 18:34:13 -0400

    feat(#10): Add binary-based memory size units

--- src/stuom/memory_size/__init__.py
+++ src/stuom/memory_size/__init__.py
@@ -2,4 +2,5 @@
 
 from .memory_size import MemorySize, Bytes  # noqa: I001
 from .decimal_memory_size import KiloBytes, MegaBytes, GigaBytes, TeraBytes, PetaBytes# noqa: I001
+from .binary_memory_size import KibiBytes, MebiBytes, GibiBytes, TebiBytes, PebiBytes# noqa: I001
 

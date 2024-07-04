ce541b76ef6fa45a062630f6d5e13e1f27f40a69
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2024-07-04 18:33:39 -0400

    test(#10): Check string formatting of decimal mem size units

--- tests/memory_size/test_decimal_memory_sizes.py
+++ tests/memory_size/test_decimal_memory_sizes.py
@@ -17,3 +17,18 @@ def test_bytes_to_terabytes_conversion():
 
 def test_bytes_to_petabytes_conversion():
     assert PetaBytes.from_memory_size(Bytes(1e15)) == PetaBytes(1)
+
+def test_string_format_kilobytes():
+    assert str(KiloBytes(20)) == "20.0 kB"
+
+def test_string_format_megabytes():
+    assert str(MegaBytes(20)) == "20.0 MB"
+
+def test_string_format_gigabytes():
+    assert str(GigaBytes(20)) == "20.0 GB"
+
+def test_string_format_terabytes():
+    assert str(TeraBytes(20)) == "20.0 TB"
+
+def test_string_format_petabytes():
+    assert str(PetaBytes(20)) == "20.0 PB"

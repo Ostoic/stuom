73210f43ddd65376f638c965dac0f75b5fb734ac
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2024-07-04 18:34:55 -0400

    test(#10): Check string formatting of binary mem size units

--- tests/memory_size/test_binary_memory_sizes.py
+++ tests/memory_size/test_binary_memory_sizes.py
@@ -19,3 +19,17 @@ def test_bytes_to_tebibytes_conversion():
 def test_bytes_to_pebibytes_conversion():
     assert PebiBytes.from_memory_size(Bytes(2**50)) == pytest.approx(PebiBytes(1))
 
+def test_string_format_kibibytes():
+    assert str(KibiBytes(20)) == "20.0 kiB"
+
+def test_string_format_mebibytes():
+    assert str(MebiBytes(20)) == "20.0 MiB"
+
+def test_string_format_gibibytes():
+    assert str(GibiBytes(20)) == "20.0 GiB"
+
+def test_string_format_tebibytes():
+    assert str(TebiBytes(20)) == "20.0 TiB"
+
+def test_string_format_pebibytes():
+    assert str(PebiBytes(20)) == "20.0 PiB"
